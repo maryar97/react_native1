2aa005e3d442b602e0bc94d0c4036e8d46f52add
Mary <marydiva201170@gmail.com>
2023-12-22 11:09:51 +0100

    notif

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CategoriesList from './CategoriesList';
+import SousCategoriesList from './SousCategoriesList';
+import ProductsList from './ProductsList';
+import ProductDetailList from './ProductDetailList';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Categories">
+        <Stack.Screen name="Categories" component={CategoriesList} />
+        <Stack.Screen name="Produits" component={ProductsList} />
+        {/* <Stack.Screen name="ProductDetail" component={ProductDetailList} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
